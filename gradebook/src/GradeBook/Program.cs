0a839c05a1994233cd4b73e280da3a419ace26b3
hugh-dickens <dickenshugh@yahoo.co.uk>
2021-10-27 17:33:55 +0100

    End of day commit

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,14 +7,15 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {   
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
+
+            var book = new Book("Hugh's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
+
         }
     }
+
+    
 }
